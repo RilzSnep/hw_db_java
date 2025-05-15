00d2ce24e52bcaf30a247e0782efddc34daf7df8
RilzSnep <fff.fff.fff.08.fff@mail.ru>
2025-05-15 10:36:56 +0300

    Управление схемами баз данных

---
+++ scripts421.sql
@@ -0,0 +1,16 @@
+
+ALTER TABLE student
+ADD CONSTRAINT check_student_age CHECK (age >= 16);
+
+ALTER TABLE student
+ADD CONSTRAINT unique_student_name UNIQUE (name);
+
+ALTER TABLE student
+ALTER COLUMN name SET NOT NULL;
+
+ALTER TABLE faculty
+ADD CONSTRAINT unique_name_color UNIQUE (name, color);
+
+
+ALTER TABLE student
+ALTER COLUMN age SET DEFAULT 20;
